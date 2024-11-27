9090799279fbb7fdb0503c7ab519cd9a976b36a5
baimisaac <ibrahimisaaceg@gmail.com>
2024-11-27 16:40:56 +0800

    Initial Commit

---
+++ FlowerDetection/FlowerDetectionApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlowerDetectionApp.swift
+//  FlowerDetection
+//
+//  Created by STDC_13 on 27/11/2024.
+//
+
+import SwiftUI
+
+@main
+struct FlowerDetectionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
